5e45907acffd6e56983b805edf6a8a85bb55c622
Anouck Galoppin <anouck.galop@hotmail.com>
2025-03-19 18:24:09 +0100

    fix description collapsible

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,6 @@
 import type { Todo } from "@/types/todoType";
 import type { Category } from "@/types/categoryType";
-import { Pencil } from "lucide-react";
-import { X } from "lucide-react";
+import { Pencil, X } from "lucide-react";
 import { Button } from "./ui/button";
 import { useTodoActions } from "@/hooks/useTodoActions";
 import {
@@ -9,7 +8,6 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
-import { ChevronDown } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 
 type PropType = {
@@ -27,35 +25,33 @@ const categoryColors: Record<string, string> = {
 };
 
 const TodoItem = ({ todo, category }: PropType) => {
-  const { handleRemoveTodo } = useTodoActions();
-  const { handleToggleTodo } = useTodoActions();
+  const { handleRemoveTodo, handleToggleTodo } = useTodoActions();
   return (
-    <li className="flex flex-col rounded-md border-2 border-solid border-gray-300 p-2 transition-all">
-      <div className="flex w-full items-center gap-5">
-        <Checkbox
-          checked={todo.completed}
-          onCheckedChange={() => handleToggleTodo(todo)}
-        />
-        <p className="mr-auto">{todo.text}</p>
-        <p
-          className={`rounded-md px-2 font-semibold ${categoryColors[category.name]}`}
-        >
-          {category.name}
-        </p>
-        <Pencil />
-        <Button onClick={() => handleRemoveTodo(todo.id)}>
-          <X />
-        </Button>
-        <Collapsible>
-          <CollapsibleTrigger className="flex items-center">
-            <ChevronDown />
-          </CollapsibleTrigger>
-          <CollapsibleContent className="w-full border-t border-gray-300 bg-gray-100 p-2 transition-all">
-            <p>{todo.description}</p>
-          </CollapsibleContent>
-        </Collapsible>
-      </div>
-    </li>
+    <Collapsible>
+      <li className="rounded-md border-2 border-gray-300 transition-all">
+        <CollapsibleTrigger className="flex w-full flex-col p-2 transition-all hover:bg-gray-100">
+          <div className="flex items-center gap-5">
+            <Checkbox
+              checked={todo.completed}
+              onCheckedChange={() => handleToggleTodo(todo)}
+            />
+            <p className="mr-auto">{todo.text}</p>
+            <p
+              className={`rounded-md px-2 font-semibold ${categoryColors[category.name]}`}
+            >
+              {category.name}
+            </p>
+            <Pencil />
+            <Button onClick={() => handleRemoveTodo(todo.id)} variant="ghost">
+              <X />
+            </Button>
+          </div>
+        </CollapsibleTrigger>
+        <CollapsibleContent className="border-t border-gray-300 bg-gray-100 p-2">
+          <p>{todo.description}</p>
+        </CollapsibleContent>
+      </li>
+    </Collapsible>
   );
 };
 
